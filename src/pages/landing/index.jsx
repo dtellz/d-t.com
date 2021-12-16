f5290dd674f57eddd2096586b1fad88bd73c3657
Diego <dtellez95@gmail.com>
2021-12-16 00:48:02 +0000

    arq: react icons deletion for testing production crash

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -8,9 +8,9 @@ import { Icon, IconButton } from '@chakra-ui/react'
 import esImg from '../../assets/language/es-lang.png'
 import enImg from '../../assets/language/en-lang.png'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
-import { GiForwardSun, GiMoonBats, GiHeraldicSun } from "react-icons/gi";
+/* import { GiForwardSun, GiMoonBats, GiHeraldicSun } from "react-icons/gi";
 import { FaSun } from "react-icons/fa";
-import { HiLightBulb } from "react-icons/hi";
+import { HiLightBulb } from "react-icons/hi"; */
 
 function Landing() {
 
@@ -67,7 +67,8 @@ function Landing() {
                     <img onClick={switchLanguageES} alt='es_language' src={esImg} height='25' width='25' className='landing__btn'></img>
                     <img onClick={switchLanguageEN} alt='en_language' src={enImg} height='25' width='25' className='landing__btn'></img>
                 </div>
-                <IconButton borderRadius='50%' aria-label='dark mode switch' color='black' className={darkMode ? 'landing__darkMode-btn' : 'landing__darkMode-btn-light'} onClick={handleMode} icon={darkMode ? <Icon w={25} h={25} as={HiLightBulb} /> : <Icon w={30} h={30} as={GiMoonBats} />} />
+                <IconButton borderRadius='50%' aria-label='dark mode switch' color='black' className={darkMode ? 'landing__darkMode-btn' : 'landing__darkMode-btn-light'} onClick={handleMode} icon={darkMode ? <SunIcon /> : <MoonIcon />} />
+                {/*                 <IconButton borderRadius='50%' aria-label='dark mode switch' color='black' className={darkMode ? 'landing__darkMode-btn' : 'landing__darkMode-btn-light'} onClick={handleMode} icon={darkMode ? <Icon w={25} h={25} as={HiLightBulb} /> : <Icon w={30} h={30} as={GiMoonBats} />} /> */}
             </header>
         </div>
     );
