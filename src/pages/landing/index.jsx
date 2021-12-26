17a5d34d366ba8f722cd37c61cc5d50239029332
Diego <dtellez95@gmail.com>
2021-12-26 13:06:16 +0000

    fix: lang switch img size

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -7,9 +7,7 @@ import esImg from '../../assets/language/es-lang.webp'
 import enImg from '../../assets/language/en-lang.webp'
 import { Link } from 'react-router-dom';
 
-/* import { GiForwardSun, GiMoonBats, GiHeraldicSun } from "react-icons/gi";
-import { FaSun } from "react-icons/fa";
-import { HiLightBulb } from "react-icons/hi"; */
+
 
 
 function Landing() {
@@ -22,11 +20,9 @@ function Landing() {
 
     const switchLanguageES = () => {
         setLanguage.changeLanguage('es')
-        /*    setAnchorEl3(null); */
     }
     const switchLanguageEN = () => {
         setLanguage.changeLanguage('en')
-        /*     setAnchorEl3(null); */
     }
 
 
@@ -62,11 +58,11 @@ function Landing() {
 
 
                 <div className='landing__lang-btns'>
-                    <img onClick={switchLanguageES} alt='es_language' src={esImg} height='30' width='30' className='landing__btn'></img>
-                    <img onClick={switchLanguageEN} alt='en_language' src={enImg} height='30' width='30' className='landing__btn'></img>
+                    <img onClick={switchLanguageES} alt='es_language' src={esImg} height='25' width='25' className='landing__btn'></img>
+                    <img onClick={switchLanguageEN} alt='en_language' src={enImg} height='25' width='25' className='landing__btn'></img>
 
                 </div>
-                {/* <IconButton borderRadius='50%' aria-label='dark mode switch' color='black' className={darkMode ? 'landing__darkMode-btn' : 'landing__darkMode-btn-light'} onClick={handleMode} icon={darkMode ? <SunIcon w={25} h={25} /> : <MoonIcon w={25} h={25} />} /> */}
+
 
             </header>
         </div >
