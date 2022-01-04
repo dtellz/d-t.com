5b5adf82290db382e50e0451e68519152ed94fc0
Diego <dtellez95@gmail.com>
2022-01-04 18:36:02 +0000

    feat: centered links

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -8,9 +8,9 @@ const Projects = () => {
     const darkMode = theme.state.darkMode;
     return (
         <div className={darkMode ? 'about__container container-dark' : 'about__container container-light'}>
-            <ul>
-                <li><a className={darkMode ? 'App-link' : 'App-link light'} href='https://stream-me-up.netlify.app'>webRTC - Live video chat app</a></li>
-            </ul>
+
+            <a className={darkMode ? 'App-link' : 'App-link light'} href='https://stream-me-up.netlify.app'>webRTC - Live video chat app</a>
+
         </div>
     )
 }
