450f92f29247c760367481fa8069a6f83c1a292c
Diego <dtellez95@gmail.com>
2022-01-07 17:29:32 +0000

    feat: project links spacing

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -9,8 +9,8 @@ const Projects = () => {
     return (
         <div className={darkMode ? 'about__container container-dark' : 'about__container container-light'}>
 
-            <a className={darkMode ? 'App-link' : 'App-link light'} href='https://stream-me-up.netlify.app'>webRTC - Live video chat app</a>
-            <a className={darkMode ? 'App-link' : 'App-link light'} href='https://versecrypto.netlify.app'>CryptoVerse - Cryptocurrencies information site</a>
+            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://stream-me-up.netlify.app'>webRTC - Live video chat app</a>
+            <a className={darkMode ? 'App-link' : 'App-link light'} href='https://versecrypto.netlify.app'>CryptoVerse - Crypto information site</a>
 
 
 
