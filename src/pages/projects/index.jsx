19608da1e25be7c20f47c61aaab93e8d1130d67b
Diego <dtellez95@gmail.com>
2022-10-30 22:31:10 +0000

    separate links

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -10,9 +10,9 @@ const Projects = () => {
         <div className={darkMode ? 'about__container container-dark' : 'about__container container-light'}>
 
             <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://stream-me-up.netlify.app'>webRTC - Live video chat app</a>
-            <a className={darkMode ? 'App-link' : 'App-link light'} href='https://versecrypto.netlify.app'>CryptoVerse - Crypto information site</a>
-            <a className={darkMode ? 'App-link' : 'App-link light'} href='https://minecraft-be.netlify.app'>Minecraft clone - Browser edition</a>
-            <a className={darkMode ? 'App-link' : 'App-link light'} href='https://dtellz.github.io/self-driving-car/'>Self driving car - Reinforcement learning neural network with vanillaJs</a>
+            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://versecrypto.netlify.app'>CryptoVerse - Crypto information site</a>
+            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://minecraft-be.netlify.app'>Minecraft clone - Browser edition</a>
+            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://dtellz.github.io/self-driving-car/'>Self driving car - Reinforcement learning neural network with vanillaJs</a>
 
         </div>
     )
