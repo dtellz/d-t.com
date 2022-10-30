6e93a2e352bdeb0fe1b87b7c38a71fd41986db98
Diego <dtellez95@gmail.com>
2022-10-30 22:37:35 +0000

    fix broken deploy

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -8,12 +8,10 @@ const Projects = () => {
     const darkMode = theme.state.darkMode;
     return (
         <div className={darkMode ? 'about__container container-dark' : 'about__container container-light'}>
-
             <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://stream-me-up.netlify.app'>webRTC - Live video chat app</a>
             <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://versecrypto.netlify.app'>CryptoVerse - Crypto information site</a>
             <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://minecraft-be.netlify.app'>Minecraft clone - Browser edition</a>
             <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://dtellz.github.io/self-driving-car/'>Self driving car - Reinforcement learning</a>
-
         </div>
     )
 }
