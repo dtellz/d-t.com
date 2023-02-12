6d78ee16722b61279f2c58b19b6beca06abb1c80
Diego <dtellez95@gmail.com>
2023-02-12 00:01:42 +0000

    fix issue on projects page

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
     const theme = React.useContext(ThemeContext);
     const darkMode = theme.state.darkMode;
     return (
-        <div className={darkMode ? 'about__container container-dark' : 'about__container container-light'}>
+        <div className={darkMode ? 'projects__container container-dark' : 'projects__container container-light'}>
             <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://stream-me-up.netlify.app'>webRTC - Live video chat app</a>
             <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://versecrypto.netlify.app'>CryptoVerse - Crypto information site</a>
             <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://minecraft-be.netlify.app'>Minecraft clone - Browser edition</a>
