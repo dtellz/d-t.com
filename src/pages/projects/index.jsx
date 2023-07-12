d9e14fbea47e7d89b907b4d2d35a5d2ff7ee16a7
dtellz <dtellez95@gmail.com>
2023-07-12 14:39:30 +0100

    add testwizard and open safe external links

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -8,10 +8,11 @@ const Projects = () => {
     const darkMode = theme.state.darkMode;
     return (
         <div className={darkMode ? 'projects__container container-dark' : 'projects__container container-light'}>
-            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://stream-me-up.netlify.app'>webRTC - Live video chat app</a>
-            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://versecrypto.netlify.app'>CryptoVerse - Crypto information site</a>
-            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://minecraft-be.netlify.app'>Minecraft clone - Browser edition</a>
-            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://dtellz.github.io/self-driving-car/'>Self driving car - Reinforcement learning</a>
+            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://marketplace.visualstudio.com/items?itemName=thesolutioners.testwizard' target='_blank' rel="noreferrer">TestWizard - AI unit test generation</a>
+            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://stream-me-up.netlify.app' target='_blank' rel="noreferrer">webRTC - Live video chat app</a>
+            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://versecrypto.netlify.app' target='_blank' rel="noreferrer">CryptoVerse - Crypto information site</a>
+            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://minecraft-be.netlify.app' target='_blank' rel="noreferrer">Minecraft clone - Browser edition</a>
+            <a style={{ paddingBottom: '35px' }} className={darkMode ? 'App-link' : 'App-link light'} href='https://dtellz.github.io/self-driving-car/' target='_blank' rel="noreferrer">Self driving car - Reinforcement learning</a>
         </div>
     )
 }
