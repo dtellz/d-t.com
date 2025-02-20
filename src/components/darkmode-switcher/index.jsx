15e08f47662ec249be2b44fdb7d94d78da4425b4
Diego <dtellez95@gmail.com>
2025-02-20 17:23:47 +0000

    revamped dark mode switcher

--- src/components/darkmode-switcher/index.jsx
+++ src/components/darkmode-switcher/index.jsx
@@ -6,7 +6,7 @@ import Switch from '@mui/material/Switch';
 import { useContext } from 'react';
 import { ThemeContext } from '../../theme';
 
-const Switcher = styled(Switch)(({ theme }) => ({
+const Switcher = styled(Switch)(({ theme, isDark }) => ({
     width: 62,
     height: 34,
     padding: 7,
@@ -19,19 +19,20 @@ const Switcher = styled(Switch)(({ theme }) => ({
             transform: 'translateX(22px)',
             '& .MuiSwitch-thumb:before': {
                 backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-                    '#fff',
+                    '#000000',
                 )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
             },
             '& + .MuiSwitch-track': {
                 opacity: 1,
-                backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+                backgroundColor: isDark ? 'rgba(79, 247, 1, 0.3)' : 'rgba(25, 118, 210, 0.5)',
             },
         },
     },
     '& .MuiSwitch-thumb': {
-        backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+        backgroundColor: isDark ? '#4ff701' : '#0072FF',
         width: 32,
         height: 32,
+        boxShadow:'none',
         '&:before': {
             content: "''",
             position: 'absolute',
@@ -48,13 +49,17 @@ const Switcher = styled(Switch)(({ theme }) => ({
     },
     '& .MuiSwitch-track': {
         opacity: 1,
-        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+        backgroundColor: isDark ? '#4ff701' : 'rgba(25, 118, 210, 0.5)',
         borderRadius: 20 / 2,
     },
 }));
 
-export default function DarkModeSwitcher() {
+const StyledSwitcher = React.forwardRef((props, ref) => {
+    const theme = useContext(ThemeContext);
+    return <Switcher {...props} ref={ref} isDark={theme.state.darkMode} />;
+});
 
+export default function DarkModeSwitcher() {
     const theme = useContext(ThemeContext);
     const darkMode = theme.state.darkMode;
     const handleMode = () => {
@@ -67,7 +72,7 @@ export default function DarkModeSwitcher() {
     return (
         <FormGroup>
             <FormControlLabel
-                control={<Switcher sx={{ m: 1 }} onClick={handleMode} />}
+                control={<StyledSwitcher sx={{ m: 1 }} onClick={handleMode} checked={darkMode} />}
                 label=""
             />
         </FormGroup>
